aaf70e917bf2c3e92733924cd8844ae6af2ff228
MiheerTamkhane <sarangtamkhane10@gmail.com>
2024-05-12 11:34:33 +0530

    created own npm package in common dir, published and using in backend to make it type safe.

---
+++ common/src/index.ts
@@ -0,0 +1,37 @@
+import z from "zod";
+
+export const signupInput = z.object({
+  email: z.string().email(),
+  password: z.string(),
+  name: z.string().optional(),
+});
+
+export type SignupType = z.infer<typeof signupInput>;
+
+export const signinInput = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export type SigninType = z.infer<typeof signinInput>;
+
+// createPostInput / CreatePostInput
+
+export const createPostInput = z.object({
+  title: z.string(),
+  content: z.string(),
+  published: z.boolean(),
+  authorId: z.string(),
+});
+
+export type CreatePostInput = z.infer<typeof createPostInput>;
+// updatePostInput / UpdatePostInput
+
+export const updatePostInput = z.object({
+  id: z.string(),
+  title: z.string(),
+  content: z.string(),
+  published: z.boolean(),
+});
+
+export type UpdatePostInput = z.infer<typeof updatePostInput>;
